a74c2b0ef033c36487dba647a1dd2ca0e80d5a59
Nibardo Reyes <nibardoreyesfelixx@gmail.com>
2026-07-10 10:24:39 -0700

    added aerospace

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,39 +1,16 @@
 return {
   {
-    "AlphaTechnolog/pywal.nvim",
+    "projekt0n/github-nvim-theme",
     lazy = false,
     priority = 1000,
     config = function()
-      local pywal = require("pywal")
-      pywal.setup()
-      vim.cmd("colorscheme pywal")
-      
-      -- Make background transparent
-      local function apply_transparency()
-        vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
-        vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE" })
-        -- Ensure line numbers are always visible
-        local linenr = vim.api.nvim_get_hl(0, { name = "LineNr" })
-        if not linenr.fg then
-          vim.api.nvim_set_hl(0, "LineNr", { fg = "#888888", bg = "NONE" })
-          vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#888888", bg = "NONE" })
-          vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#888888", bg = "NONE" })
-        end
-      end
-      
-      apply_transparency()
-
-      -- Auto-reload when pywal colors change
-      vim.api.nvim_create_autocmd("Signal", {
-        pattern = "SIGUSR1",
-        callback = function()
-          vim.cmd("colorscheme pywal")
-          apply_transparency()
-        end,
+      require("github-theme").setup({
+        options = {
+          transparent = true,
+        },
       })
+
+      vim.cmd("colorscheme github_dark")
     end,
   },
 }
